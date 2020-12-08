cc619b21f1bcc2fadc8e0b9e7478a4570fabb86a
SC GameDesign <SCGAMEDESIGN@christchurch.natcoll.ac.nz>
2020-12-08 15:05:15 +1300

    added grande to player

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public float fuseTime = 2f;
+    public float range = 10f;
+    public float damage = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Activate());
+    }
+
+    IEnumerator Activate()
+    {
+        //count down then explode
+        yield return new WaitForSeconds(fuseTime);
+        //now explode
+        Instantiate(explosionPrefab,transform.position,Quaternion.identity);
+        DamageInRange(range);
+        Destroy(gameObject);
+    }
+
+    void DamageInRange(float range)
+    {
+        //damage all the objects with damage scripts in the given range
+        Collider[] colliders = Physics.OverlapSphere(transform.position,range);
+        foreach (var collider in colliders)
+        {
+            //check if it has a damage script
+            var damageable = collider.GetComponent<Damage>();
+            if(damageable)
+            {
+                //it has a damage script
+                float totalDamage = damage * (1f- Vector3.Distance(transform.position,collider.transform.position)/range);
+                damageable.TakeDamage(totalDamage);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerGrenadeLauncher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrenadeLauncher : MonoBehaviour
+{
+    public int startingMunerOfGrenades = 3;
+    int numGrenades  = 3;
+
+    public Rigidbody grenadePrefab;
+    public Transform spawnPosition;
+    public float launchForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numGrenades = startingMunerOfGrenades;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && numGrenades > 0)
+        {
+            numGrenades--;
+            Rigidbody rb = Instantiate(grenadePrefab, spawnPosition.position,spawnPosition.rotation);
+            rb.AddForce(spawnPosition.forward * launchForce, ForceMode.Impulse);
+        }
+    }
+}
